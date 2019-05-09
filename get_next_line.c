d91d36e60b3b80c69e798b9025972bdda8c2184e
Thomas HALLOT <thallot@e1r2p4.42.fr>
2019-05-09 15:41:09 +0200

    new version

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thallot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 17:42:19 by thallot           #+#    #+#             */
-/*   Updated: 2019/04/22 10:37:48 by thallot          ###   ########.fr       */
+/*   Updated: 2019/05/03 14:28:07 by thallot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ static char		*ft_read_line(int fd, char *str)
 	char		*tmp;
 	int			cursor;
 
-	cursor = 1;
-	while (cursor > 0)
+	while ((cursor = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
-		if ((cursor = read(fd, buffer, BUFF_SIZE)) == -1)
+		if (cursor == -1)
+		{
+			free(str);
 			return (NULL);
+		}
 		buffer[cursor] = '\0';
 		tmp = str;
 		str = ft_strjoin(str, buffer);
@@ -33,14 +35,26 @@ static char		*ft_read_line(int fd, char *str)
 	return (str);
 }
 
-static char		*ft_cut_line(char *str, char *tmp)
+static void		ft_cut_line(char **str, char **line)
 {
 	char		*ptr;
+	size_t		size;
 
-	ptr = str;
-	str = ft_strsub(str, ft_strlen(tmp) + 1, ft_strlen(str) - ft_strlen(tmp));
-	ft_memdel((void **)&ptr);
-	return (str);
+	size = 0;
+	while ((*str)[size] != '\n' && (*str)[size] != '\0')
+		size++;
+	if ((*str)[size] == '\n')
+	{
+		*line = ft_strsub(*str, 0, size);
+		ptr = ft_strdup(&((*str)[size + 1]));
+		free(*str);
+		*str = ptr;
+	}
+	else
+	{
+		*line = ft_strdup(*str);
+		ft_memdel((void**)str);
+	}
 }
 
 static int		ft_create_save(t_file **file, int index)
@@ -61,7 +75,7 @@ int				get_next_line(const int fd, char **line)
 	size_t			size;
 
 	size = 0;
-	if (fd < 0 || line == NULL || fd > OPEN_MAX)
+	if (fd < 0 || line == NULL || fd > OPEN_MAX || read(fd, 0, 0) == -1)
 		return (-1);
 	if (ft_create_save(file, fd) == -1)
 		return (-1);
@@ -69,11 +83,12 @@ int				get_next_line(const int fd, char **line)
 		file[fd]->str = ft_strnew(0);
 	if ((file[fd]->str = ft_read_line(fd, file[fd]->str)) == NULL)
 		return (-1);
-	while (file[fd]->str[size] != '\n' && file[fd]->str[size] != '\0')
-		size++;
-	*line = ft_strsub(file[fd]->str, 0, size);
-	if (file[fd]->str[0] == '\0' && *line[0] == '\0')
+	ft_cut_line(&(file[fd]->str), line);
+	if (!file[fd]->str && *line[0] == '\0')
+	{
+		free(*line);
+		*line = NULL;
 		return (0);
-	file[fd]->str = ft_cut_line(file[fd]->str, *line);
+	}
 	return (1);
 }
